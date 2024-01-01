ae4c7b220cbbdc9427b8d450c3113d2a2f767f1a
abdelrahman830a <abdelrahman.1220005@stemalex.moe.edu.eg>
2024-01-01 11:35:32 +0200

    first commit

---
+++ components/index.js
@@ -0,0 +1,15 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RedButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RedButton,
+  DetailsDesc,
+  DetailsBid,
+};
